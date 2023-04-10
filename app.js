d70a05aaa19555bbd558fd16bc1275cd1b9582a3
AdnaanH <60857004+AdnaanH@users.noreply.github.com>
2023-04-10 10:46:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,25 +8,3 @@ dropdown.forEach(function(dd_menu){
         dd_menu.classList.add("hidden");
     })
 })
-
-
-//dropdown.forEach(dd_menu => {
-   // dropdown.addEventListener('click', () => {
-        //document.querySelector('.hidden')?.classList.remove('hidden');
-        //dropdown.classList.add('hidden');
-    //});
-//});
-
-
-
-
-
-
-//const selectedSummary = document.querySelectorAll('.summary');
-
-//selectedSummary.forEach(summary => {
-  //  summary.addEventListener('click', () => {
-    //    document.querySelector('.active')?.classList.remove('active');
-      //  summary.classList.add('active');
-    //});
-//});
